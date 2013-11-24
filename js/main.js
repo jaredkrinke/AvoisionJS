48e68e80c81644ae08fbdf051f277cad7419b342
jaredkrinke <jaredkrinke@gmail.com>
2013-11-23 21:28:07 -0800

    Removed dead code

--- js/main.js
+++ js/main.js
@@ -1,78 +1,14 @@
-﻿// TODO: Load images at some point...
-var player = {
-    speed: 100,
-    x: 0,
-    y: 0
-}
-
-var keysPressed = {};
-addEventListener("keydown", function (e) {
-    keysPressed[e.keyCode] = true;
-}, false);
-
-addEventListener("keyup", function (e) {
-    delete keysPressed[e.keyCode];
-}, false);
-
-function reset() {
-    player.x = canvas.width / 2;
-    player.y = canvas.height / 2;
-}
-
-var keyNameToCode = {
-    backspace: 8,
-    tab: 9,
-    enter: 13,
-    escape: 27,
-    space: 32,
-    left: 37,
-    up: 38,
-    right: 39,
-    down: 40
+﻿var keyCodeToName = {
+    //8: 'backspace',
+    //9: 'tab',
+    //13: 'enter',
+    //27: 'escape',
+    //32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
 };
-
-var keyCodeToName = {};
-(function () {
-    for (var name in keyNameToCode) {
-        keyCodeToName[keyNameToCode[name]] = name;
-    }
-})();
-
-function update(delta) {
-    if (keyNameToCode.up in keysPressed) {
-        player.y -= player.speed * delta;
-    }
-
-    if (keyNameToCode.down in keysPressed) {
-        player.y += player.speed * delta;
-    }
-
-    if (keyNameToCode.left in keysPressed) {
-        player.x -= player.speed * delta;
-    }
-
-    if (keyNameToCode.right in keysPressed) {
-        player.x += player.speed * delta;
-    }
-}
-
-function drawFrame() {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(player.x - 16, player.y - 16, 32, 32);
-}
-
-var mainLoop = function () {
-    var now = Date.now();
-    var delta = now - then;
-    if (delta > 0) {
-        update(delta / 1000);
-    }
-    drawFrame();
-    then = now;
-    requestAnimationFrame(mainLoop);
-}
 
 // Layer that contains entities to display/update
 function Layer() {
